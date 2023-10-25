829af29805b5c902064c4e8ee1b4f0a63fb1b2e9
Jul1111 <48258028+Jul1111@users.noreply.github.com>
2023-10-25 11:45:49 +0200

    Update views.py

--- ntiers/napi/views.py
+++ ntiers/napi/views.py
@@ -8,12 +8,12 @@ def formulaire_message(request):
     return render(request, 'napi/formulaire_message.html')
 
 @csrf_exempt
-# Utilisé pour exclure cette vue spécifiquement de la protection CORS (à utiliser avec prudence)
+
 def traiter_message(request):
     if request.method == 'POST':
         contenu = request.POST.get('contenu')
 
-        # Traitement du message (à adapter selon tes besoins)
+        # Traitement du message 
 
         # Enregistrement du message dans la base de données distante
         nouveau_message = Message(contenu=contenu)
@@ -21,7 +21,7 @@ def traiter_message(request):
 
         response = JsonResponse({'message': 'Message traité avec succès'})
 
-        # Autoriser toutes les origines (à ajuster en fonction de tes besoins)
+        # Autoriser toutes les origines 
         response["Access-Control-Allow-Origin"] = "*"
 
         return response
